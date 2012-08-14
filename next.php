3c7aaa96a5e1597f722b3052209893ab3a1a341b
Jeff Sharkey <jsharkey@android.com>
2012-08-13 23:50:57 -0700

    Fix copyright date.

--- next.php
+++ next.php
@@ -1,6 +1,6 @@
 <?
 /*
-    Copyright 2012, Jeff Sharkey
+    Copyright 2007, Jeff Sharkey
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
